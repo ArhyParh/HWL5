4191fc47ef1348a9b713eb3c67eb81ff16f60cae
ArhyParh <zescoarty@gmail.com>
2023-06-03 23:31:47 +0300

    Задачи решены

--- task2/Program.cs
+++ task2/Program.cs
@@ -6,26 +6,26 @@ void Main()
 {
     Console.WriteLine("Для того, чтобы найти точку пересечения 2 прямых, необходимо ввести параметры коэффициентов: b и k.");
     Console.WriteLine("Введите их по очереди: сперва b и k  первой прямой, а затем второй...");
-    int [] first = GetArray();
-    int [] second = GetArray();
+    double [] first = GetArray();
+    double [] second = GetArray();
     Console.WriteLine(String.Join(", ",first));
     Console.WriteLine(String.Join(", ",second));
     double [] result = DoTheLineIntersect(first,second);
-    Console.WriteLine(String.Join(",",result));
+    Console.WriteLine(String.Join("|",result));
 }
-int[] GetArray()
+double[] GetArray()
 {
-    int i = 0;
-    int[] back = new int[2];
-    while(i<2){
-    back[i]= int.Parse(Console.ReadLine()!);
-    i++;
+    int i = 2;
+    double[] back = new double[2];
+    while(i!=0){
+    back[i-1]= int.Parse(Console.ReadLine()!);
+    i--;
     }
     return back;
 }
-double [] DoTheLineIntersect(int[] a1, int [] a2)
+double [] DoTheLineIntersect(double[] a1, double [] a2)
 {
-    double x = (a2[1]-a1[1])/(a1[0]-a2[1]);
+    double x = (a2[1]-a1[1])/(a1[0]-a2[0]);
     double y = (a1[0]*x +a1[1]);
     double [] back = new double[]{x,y};
     return back;
